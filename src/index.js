f0ad7db323d6aac20cb0ee5fb7e9e0146ae60e30
ManuelQHernandez <manuel.quinteros@telusinternational.com>
2022-08-25 09:59:35 -0600

    REACT APP

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import App from './Components/App';
+import ClickCounter from "./Components/ClickCounter";
+
+ReactDOM.render(<ClickCounter />, document.getElementById('app'));
